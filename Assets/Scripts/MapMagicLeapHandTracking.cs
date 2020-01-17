d4497e6a8ec70bb171810c93ba31938e4a3becec
Mikko Haapoja <me@mikkoh.com>
2020-01-17 13:21:52 -0500

    Rigging finished

---
+++ Assets/Scripts/MapMagicLeapHandTracking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class MapMagicLeapHandTracking : MonoBehaviour
+{
+    public Transform TargetLeftHand;
+    public Transform TargetRightHand;
+
+    private void Start()
+    {
+        MLHands.Start();
+    }
+
+    void Update()
+    {
+        var leftWrist = MLHands.Left.Wrist;
+        var rightWrist = MLHands.Right.Wrist;
+
+        TargetLeftHand.position = leftWrist.Center.Position;
+        TargetRightHand.position = rightWrist.Center.Position;
+    }
+
+    void OnDestroy()
+    {
+        MLHands.Stop();
+    }
+}
---
+++ Assets/Scripts/XRRig.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class XRMap
+{
+    public Transform xrTarget;
+    public Transform rigTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+
+    public void Map()
+    {
+        rigTarget.position = xrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.rotation = xrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+}
+
+public class XRRig : MonoBehaviour
+{
+    public XRMap head;
+    public XRMap leftArm;
+    public XRMap rightArm;
+    public Transform headConstraint;
+
+    private Vector3 headBodyOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        headBodyOffset = transform.position - headConstraint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = headConstraint.position + headBodyOffset;
+        transform.forward = Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized;
+
+        head.Map();
+        leftArm.Map();
+        rightArm.Map();
+    }
+}
