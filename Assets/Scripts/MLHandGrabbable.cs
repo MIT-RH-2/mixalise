ee2a920a9b6c5187d01208f36722757e5cbd5dea
Mikko Haapoja <me@mikkoh.com>
2020-01-17 17:56:26 -0500

    You can now grab items

---
+++ Assets/Scripts/MLHandGrabbable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MLHandGrabbable : MonoBehaviour
+{
+    private bool isKinematicAtGrab;
+    private Vector3 grabOffset;
+
+    private void Start()
+    {
+        if (GetComponent<Rigidbody>() == null)
+        {
+            Debug.LogError("Grabbable needs a RigidBody");
+        }
+    }
+
+    public void StartGrab(MLHandGrabber grabber)
+    {
+        var rigid = GetComponent<Rigidbody>();
+
+        if (rigid != null)
+        {
+            isKinematicAtGrab = rigid.isKinematic;
+            rigid.isKinematic = true;
+        }
+
+        grabOffset = transform.position - grabber.transform.position;
+    }
+
+    public void StopGrab(MLHandGrabber grabber)
+    {
+        var rigid = GetComponent<Rigidbody>();
+
+        if (rigid != null)
+        {
+            rigid.isKinematic = isKinematicAtGrab;
+        }
+    }
+
+    public void UpdateGrab(MLHandGrabber grabber)
+    {
+        transform.position = grabber.transform.position + grabOffset;
+    }
+}
---
+++ Assets/Scripts/MLHandGrabber.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class EventGrabUpdate : UnityEvent<bool> { }
+
+public class MLHandGrabber : MonoBehaviour
+{
+    public bool isGrabbing { get; private set; }
+    public MLHandType handType = MLHandType.Left;
+    public MLHandKeyPose grabPose = MLHandKeyPose.Fist;
+    public float minConfidence = 0.9f;
+    public EventGrabUpdate onGrabUpdate;
+
+    private MLHand hand;
+    private List<MLHandGrabbable> collidingGrabables = new List<MLHandGrabbable>();
+    private List<MLHandGrabbable> movingGrabbables;
+
+    private void Start()
+    {
+        var curCollider = GetComponent<Collider>();
+
+        if (curCollider == null)
+        {
+            Debug.LogError("Grabber should have a collider");
+        }
+
+        curCollider.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(handType == MLHandType.Left)
+        {
+            hand = MLHands.Left;
+        } else
+        {
+            hand = MLHands.Right;
+        }
+
+        CheckGrabGesture();
+
+        if (isGrabbing)
+        {
+            movingGrabbables.ForEach((grabable) => { grabable.UpdateGrab(this); });
+        }
+    }
+
+    void CheckGrabGesture()
+    {
+        if (hand == null)
+        {
+            return;
+        }
+
+        var isDoingGrabGesture = hand.KeyPose == grabPose;
+
+        if (isDoingGrabGesture != isGrabbing)
+        {
+            isGrabbing = isDoingGrabGesture;
+            onGrabUpdate.Invoke(isGrabbing);
+
+            if (isGrabbing)
+            {
+                movingGrabbables = new List<MLHandGrabbable>(collidingGrabables);
+                Debug.Log(movingGrabbables.Count + " " + collidingGrabables.Count);
+                movingGrabbables.ForEach((grabable) => { grabable.StartGrab(this); });
+            } else
+            {
+                movingGrabbables.ForEach((grabable) => { grabable.StopGrab(this); });
+                movingGrabbables = null;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider collidingItem)
+    {
+        Debug.Log("ON TRIGGER ENTER");
+
+        var grabbable = collidingItem.gameObject.GetComponent<MLHandGrabbable>();
+
+        if (grabbable == null)
+        {
+            return;
+        }
+
+        collidingGrabables.Add(grabbable);
+    }
+
+    void OnTriggerExit(Collider collidingItem)
+    {
+        var grabbable = collidingItem.gameObject.GetComponent<MLHandGrabbable>();
+
+        if (grabbable == null)
+        {
+            return;
+        }
+
+        collidingGrabables.Remove(grabbable);
+    }
+}
