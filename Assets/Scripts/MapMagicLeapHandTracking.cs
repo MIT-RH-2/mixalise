612d835927c266944bf05301a20496d9c6158f72
Mikko Haapoja <me@mikkoh.com>
2020-01-18 13:44:03 -0500

    Hand rotation is now correct

--- Assets/Scripts/MapMagicLeapHandTracking.cs
+++ Assets/Scripts/MapMagicLeapHandTracking.cs
@@ -5,12 +5,37 @@ using UnityEngine.XR.MagicLeap;
 
 public class MapMagicLeapHandTracking : MonoBehaviour
 {
+    public const float FPS_60 = 0.016666666666667f;
+
     public Transform targetLeftHand;
     public Transform targetRightHand;
+    public float handRotationEase = 0.1f;
+    public GameObject debugPrefab;
+
+    private Transform debugHandStart;
+    private Transform debugHandUp;
 
     private void Start()
     {
         MLHands.Start();
+
+        if (debugPrefab != null)
+        {
+            var deubgScale = 0.1f;
+            var debugBallScale = new Vector3(deubgScale, deubgScale, deubgScale);
+
+            var startDebug = Instantiate(debugPrefab);
+            startDebug.name = "startDebug";
+
+            var upDebug = Instantiate(debugPrefab);
+            upDebug.name = "upDebug";
+
+            debugHandStart = startDebug.transform;
+            debugHandUp = upDebug.transform;
+
+            debugHandStart.localScale = debugBallScale;
+            debugHandUp.localScale = debugBallScale;
+        }
     }
 
     void OnDestroy()
@@ -22,13 +47,11 @@ public class MapMagicLeapHandTracking : MonoBehaviour
     {
         if (MLHands.Left != null)
         {
-            Debug.Log("left" + MLHands.Left.Index.KeyPoints[0].Position);
             UpdateHand(MLHands.Left, targetLeftHand);
         }
 
         if (MLHands.Right != null)
         {
-            Debug.Log("right" + MLHands.Right.Index.KeyPoints[0].Position);
             UpdateHand(MLHands.Right, targetRightHand);
         }
     }
@@ -37,12 +60,26 @@ public class MapMagicLeapHandTracking : MonoBehaviour
     {
         target.position = hand.Wrist.Center.Position;
 
-        var centerToUlnar = hand.Wrist.Ulnar.Position - hand.Center;
-        var centerToRadial = hand.Wrist.Radial.Position - hand.Center;
-        var centerToCenter = hand.Wrist.Center.Position - hand.Center;
-        var handUp = Vector3.Cross(centerToUlnar, centerToRadial);
+        var a = hand.Wrist.Center.Position;
+        var b = hand.Index.KeyPoints[0].Position;
+        var c = hand.Middle.KeyPoints[0].Position;
 
-        var rotation = Quaternion.LookRotation(centerToCenter, handUp);
-        target.rotation = rotation;
+        var aToB = b - a;
+        var aToC = c - a;
+        var centerToCenter = hand.Wrist.Center.Position - a;
+        var handUp = Vector3.Cross(aToB, aToC).normalized;
+
+        //var rotation = Quaternion.LookRotation(handUp, Vector3.up);
+        var rotation = Quaternion.LookRotation(a - b, handUp);
+        target.rotation = Quaternion.Slerp(target.rotation, rotation, handRotationEase);
+
+        if (debugHandStart)
+        {
+            debugHandStart.position = a;
+            debugHandUp.position = a + handUp;
+
+            Debug.DrawLine(debugHandStart.position, debugHandUp.position, new Color(1, 0, 0, 1));
+        }
+        
     }
 }
