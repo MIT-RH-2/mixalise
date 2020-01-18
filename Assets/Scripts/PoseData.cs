202014cf816a152152c60cf7e6d12de1fa87f08c
Ben Clarke <me@benclarke.ca>
2020-01-17 21:56:37 -0500

    Add generic PoseListener

---
+++ Assets/Scripts/PoseData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.MagicLeap;
+
+public class PoseData {
+
+    public enum PoseAction {
+        Started,
+        Completed
+    }
+
+    public PoseAction action { get; private set; }
+    public MLHandKeyPose pose { get; private set; }
+
+    public PoseData(PoseAction action, MLHandKeyPose pose) {
+        this.action = action;
+        this.pose = pose;
+    }
+
+}
---
+++ Assets/Scripts/PoseListener.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.MagicLeap;
+
+[Serializable]
+public class PoseEvent : UnityEvent<PoseData>
+{
+
+}
+
+public class PoseListener : MonoBehaviour
+{
+
+    private Coroutine timeout;
+
+    public float TimeoutSeconds = 2f;
+
+    public MLHandKeyPose Pose = MLHandKeyPose.Ok;
+    public MLHand hand;
+
+    public float MinConfidence = 0.9f;
+
+    IEnumerator poseTimeout;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (hand.KeyPose == Pose && hand.KeyPoseConfidence >= MinConfidence) {
+            this.timeout = StartCoroutine(this.PoseDetected());
+        } else {
+            StopCoroutine(this.timeout);
+        }
+
+    }
+
+    IEnumerator PoseDetected() {
+
+        PoseData data = new PoseData(PoseData.PoseAction.Started, this.Pose);
+        new PoseEvent().Invoke(data);
+
+        yield return new WaitForSeconds(TimeoutSeconds);
+
+        data = new PoseData(PoseData.PoseAction.Completed, this.Pose);
+        new PoseEvent().Invoke(data);
+
+    }
+}
