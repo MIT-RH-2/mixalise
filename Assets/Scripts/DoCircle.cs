4ebed99fbf73a3633d4b580892067e2211e4b7ae
Ben Clarke <me@benclarke.ca>
2020-01-17 20:50:09 -0500

    Record serialized stream of transform changes

---
+++ Assets/Scripts/DoCircle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoCircle : MonoBehaviour
+{
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Cos(Time.time) * 5f,
+            0f,
+            Mathf.Sin(Time.time) * 5f
+        );
+    }
+}
