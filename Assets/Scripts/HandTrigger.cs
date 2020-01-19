90edc9dc64bde092808feca54601a8ad238047a4
Mikko Haapoja <me@mikkoh.com>
2020-01-19 11:06:43 -0500

    Implemented playing the airhorn when your hand hits it in the recorder scene

---
+++ Assets/Scripts/HandTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ColliderToEvent {
+    public Collider collider;
+    public UnityEvent onTrigger;
+}
+
+public class HandTrigger : MonoBehaviour
+{
+    public List<ColliderToEvent> colliderAndEvents;
+
+    void OnTriggerEnter(Collider collidingItem)
+    {
+        if (colliderAndEvents != null) {
+            colliderAndEvents.ForEach((colliderToEvent) => {
+                if (colliderToEvent.collider == collidingItem) {
+                    colliderToEvent.onTrigger.Invoke();
+                }
+            });
+        }
+    }
+}
